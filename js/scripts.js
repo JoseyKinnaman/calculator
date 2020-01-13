d34b436dca0e599117ad1f4ca7bcf9f3abd942da
Dana Luukko and Jozy Kinnaman <jozy@>
2020-01-13 15:30:55 -0800

    Add temp function.

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,12 @@ var number2 = parseInt(prompt("Enter your height in inches "));
 var result = BMI(number1,number2);
 
 alert(result);
+
+var Temp = function (number1){
+    return ((number1 * 9/5) + 32)
+};
+
+var number1 = parseInt (prompt ("Enter the temp in Celsius "));
+var result = Temp(number1);
+
+alert(result);
